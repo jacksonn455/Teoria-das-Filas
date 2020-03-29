d724fc154147d5a36a9dad96bced759b06ba23b3
Jackson Magnabosco <jacksonmagnabosco@hotmail.com>
2020-03-29 18:59:29 -0300

    validação de campos feita

--- js/formulario.js
+++ js/formulario.js
@@ -2,121 +2,135 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
 
-    var tempo = document.querySelector("#tempo").value;
-    var chegadaA = document.querySelector("#chegadaA").value;
-    var chegadaB = document.querySelector("#chegadaB").value;
-    var chegadaC = document.querySelector("#chegadaC").value;
-    var servicoA = document.querySelector("#servicoA").value;
-    var servicoB = document.querySelector("#servicoB").value;
-    var servicoC = document.querySelector("#servicoC").value;
-    var clienteProb = document.querySelector("#clienteProb").value;
+    var erros = validar();
 
-    RespChegadaC = calcularResC(chegadaC, tempo);
-    RespChegadaB = calcularResB(chegadaB, tempo);
-    RespChegadaA = calcularResA(chegadaA, tempo);
-    RespServicoA = calcularResA(servicoA, tempo);
-    RespServicoB = calcularResB(servicoB, tempo);
-    RespServicoC = calcularResC(servicoC, tempo);
-    MediaA = calcMediaA(RespServicoA, RespChegadaA);
-    MediaB = calcMediaB(RespServicoB, RespChegadaB);
-    MediaC = calcMediaC(RespServicoC, RespChegadaC);
-    MediaSistemaA = calcMediaSistA(RespChegadaA, RespServicoA);
-    MediaSistemaB = calcMediaSistB(RespChegadaB, RespServicoB);
-    MediaSistemaC = calcMediaSistC(RespChegadaC, RespServicoC);
-    TaxamediaSisA = calcTaxaMediaA(RespChegadaA, RespServicoA);
-    TaxamediaSisB = calcTaxaMediaB(RespChegadaB, RespServicoB);
-    TaxamediaSisC = calcTaxaMediaC(RespChegadaC, RespServicoC);
+    if (erros.length > 0) {
+        exibeMensagensDeErro(erros);
+        return;
+    }
 
-    var chegadaRespostaA = document.querySelector("#RespChegadaA");
-    chegadaRespostaA.value  = RespChegadaA
+    if (erros.length == 0) {
+        var tempo = document.querySelector("#tempo").value;
+        var chegadaA = document.querySelector("#chegadaA").value;
+        var chegadaB = document.querySelector("#chegadaB").value;
+        var chegadaC = document.querySelector("#chegadaC").value;
+        var servicoA = document.querySelector("#servicoA").value;
+        var servicoB = document.querySelector("#servicoB").value;
+        var servicoC = document.querySelector("#servicoC").value;
+        var clienteProb = document.querySelector("#clienteProb").value;
 
-    var chegadaRespostaB = document.querySelector("#RespChegadaB");
-    chegadaRespostaB.value  = RespChegadaB
+        RespChegadaC = calcularResC(chegadaC, tempo);
+        RespChegadaB = calcularResB(chegadaB, tempo);
+        RespChegadaA = calcularResA(chegadaA, tempo);
+        RespServicoA = calcularResA(servicoA, tempo);
+        RespServicoB = calcularResB(servicoB, tempo);
+        RespServicoC = calcularResC(servicoC, tempo);
+        MediaA = calcMediaA(RespServicoA, RespChegadaA);
+        MediaB = calcMediaB(RespServicoB, RespChegadaB);
+        MediaC = calcMediaC(RespServicoC, RespChegadaC);
+        MediaSistemaA = calcMediaSistA(RespChegadaA, RespServicoA);
+        MediaSistemaB = calcMediaSistB(RespChegadaB, RespServicoB);
+        MediaSistemaC = calcMediaSistC(RespChegadaC, RespServicoC);
+        TaxamediaSisA = calcTaxaMediaA(RespChegadaA, RespServicoA);
+        TaxamediaSisB = calcTaxaMediaB(RespChegadaB, RespServicoB);
+        TaxamediaSisC = calcTaxaMediaC(RespChegadaC, RespServicoC);
 
-    var chegadaRespostaC = document.querySelector("#RespChegadaC");
-    chegadaRespostaC.value  = RespChegadaC
+        var chegadaRespostaA = document.querySelector("#RespChegadaA");
+        chegadaRespostaA.value = RespChegadaA;
 
-    var servicoRespostaA = document.querySelector("#RespServA");
-    servicoRespostaA.value  = RespServicoA
+        var chegadaRespostaB = document.querySelector("#RespChegadaB");
+        chegadaRespostaB.value = RespChegadaB;
 
-    var servicoRespostaB = document.querySelector("#RespServB");
-    servicoRespostaB.value = RespServicoB
+        var chegadaRespostaC = document.querySelector("#RespChegadaC");
+        chegadaRespostaC.value = RespChegadaC;
+
+        var servicoRespostaA = document.querySelector("#RespServA");
+        servicoRespostaA.value = RespServicoA;
+
+        var servicoRespostaB = document.querySelector("#RespServB");
+        servicoRespostaB.value = RespServicoB;
 
 
-    var servicoRespostaC = document.querySelector("#RespServC");
-    servicoRespostaC.value = RespServicoC
+        var servicoRespostaC = document.querySelector("#RespServC");
+        servicoRespostaC.value = RespServicoC;
 
-    var carroA = document.querySelector("#carroA");
-    carroA.value = MediaA;
+        var carroA = document.querySelector("#carroA");
+        carroA.value = MediaA;
 
-    var carroB = document.querySelector("#carroB");
-    carroB.value = MediaB;
+        var carroB = document.querySelector("#carroB");
+        carroB.value = MediaB;
 
-    var carroC = document.querySelector("#carroC");
-    carroC.value = MediaC;
+        var carroC = document.querySelector("#carroC");
+        carroC.value = MediaC;
 
-    var taxaMediaA = document.querySelector("#tempoA");
-    taxaMediaA.value = -MediaSistemaA
+        var taxaMediaA = document.querySelector("#tempoA");
+        taxaMediaA.value = -MediaSistemaA;
 
-    var taxaMediaB = document.querySelector("#tempoB");
-    taxaMediaB.value = -MediaSistemaB
+        var taxaMediaB = document.querySelector("#tempoB");
+        taxaMediaB.value = -MediaSistemaB;
 
-    var taxaMediaC = document.querySelector("#tempoC");
-    taxaMediaC.value = -MediaSistemaC
+        var taxaMediaC = document.querySelector("#tempoC");
+        taxaMediaC.value = -MediaSistemaC;
 
-    var taxaSistMediaA = document.querySelector("#taxaMediA");
-    taxaSistMediaA.value = TaxamediaSisA 
+        var taxaSistMediaA = document.querySelector("#taxaMediA");
+        taxaSistMediaA.value = TaxamediaSisA;
 
-    var taxaSistMediaB = document.querySelector("#taxaMediB");
-    taxaSistMediaB.value = TaxamediaSisB 
+        var taxaSistMediaB = document.querySelector("#taxaMediB");
+        taxaSistMediaB.value = TaxamediaSisB;
 
-    var taxaSistMediaC = document.querySelector("#taxaMediC");
-    taxaSistMediaC.value = TaxamediaSisC 
+        var taxaSistMediaC = document.querySelector("#taxaMediC");
+        taxaSistMediaC.value = TaxamediaSisC;
+    
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
+    mensagensErro.value = "";
+    }
+
 
 });
 
-function calcTaxaMediaA(RespChegadaA, RespServicoA){
-    var media =  (RespChegadaA / RespServicoA);
+function calcTaxaMediaA(RespChegadaA, RespServicoA) {
+    var media = (RespChegadaA / RespServicoA);
     return media.toFixed(2);
 }
 
-function calcTaxaMediaB(RespChegadaB, RespServicoB){
-    var media =  (RespChegadaB / RespServicoB);
+function calcTaxaMediaB(RespChegadaB, RespServicoB) {
+    var media = (RespChegadaB / RespServicoB);
     return media.toFixed(2);
 }
 
 
-function calcTaxaMediaC(RespChegadaC, RespServicoC){
-    var media =  (RespChegadaC / RespServicoC);
+function calcTaxaMediaC(RespChegadaC, RespServicoC) {
+    var media = (RespChegadaC / RespServicoC);
     return media.toFixed(2);
 }
 
-function calcMediaSistA(RespChegadaA, RespServicoA){
+function calcMediaSistA(RespChegadaA, RespServicoA) {
     var media = 1 / (RespChegadaA - RespServicoA);
     return media.toFixed(2);
 }
 
-function calcMediaSistB(RespChegadaB, RespServicoB){
+function calcMediaSistB(RespChegadaB, RespServicoB) {
     var media = 1 / (RespChegadaB - RespServicoB);
     return media.toFixed(2);
 }
 
-function calcMediaSistC(RespChegadaC, RespServicoC){
+function calcMediaSistC(RespChegadaC, RespServicoC) {
     var media = 1 / (RespChegadaC - RespServicoC);
     return media.toFixed(2);
 }
 
-function calcMediaA(RespChegadaA, RespServicoA){
+function calcMediaA(RespChegadaA, RespServicoA) {
     var media = RespServicoA / (RespChegadaA - RespServicoA);
     return media.toFixed(2);
 }
 
-function calcMediaB(RespChegadaB, RespServicoB){
+function calcMediaB(RespChegadaB, RespServicoB) {
     var media = RespServicoB / (RespChegadaB - RespServicoB);
     return media.toFixed(2);
 }
 
-function calcMediaC(RespChegadaC, RespServicoC){
+function calcMediaC(RespChegadaC, RespServicoC) {
     var media = RespServicoC / (RespChegadaC - RespServicoC);
     return media.toFixed(2);
 }
@@ -157,4 +171,56 @@ function calcularResA(chegadaA, tempo) {
     var RespChegadaA = 0;
     RespChegadaA = tempo / chegadaA;
     return RespChegadaA.toFixed(2);
+}
+
+function exibeMensagensDeErro(erros) {
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+
+    erros.forEach(function (erro) {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
+
+function validar() {
+
+    var erros = [];
+
+    var tempo = formularioAdiciona.tempo.value;
+    var chegadaA = formularioAdiciona.chegadaA.value;
+    var chegadaB = formularioAdiciona.chegadaB.value;
+    var chegadaC = formularioAdiciona.chegadaC.value;
+    var servicoA = formularioAdiciona.servicoA.value;
+    var servicoB = formularioAdiciona.servicoB.value;
+    var servicoC = formularioAdiciona.servicoC.value;
+    var clienteProb = formularioAdiciona.clienteProb.value;
+
+    if (tempo.length == 0) {
+        erros.push("O tempo não pode ser em branco");
+    }
+    if (chegadaA.length == 0) {
+        erros.push("A chegada (A) não pode ser em branco");
+    }
+    if (chegadaB.length == 0) {
+        erros.push("A chegada (B) não pode ser em branco");
+    }
+    if (chegadaC.length == 0) {
+        erros.push("A chegada (C) não pode ser em branco");
+    }
+    if (servicoA.length == 0) {
+        erros.push("O serviço (A) não pode ser em branco");    
+    }
+    if (servicoB.length == 0) {
+        erros.push("O serviço (B) não pode ser em branco");
+    }
+    if (servicoC.length == 0) {
+        erros.push("O serviço (C) não pode ser em branco");
+    }
+    if (clienteProb.length == 0) {
+        erros.push("O cliente não pode ser em branco");
+    }
+
+    return erros;
 }
